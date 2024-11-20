15851d513f757e4275437d248f233a4b213f9dd0
oussamaamou <amoussdark@gmail.com>
2024-11-20 21:07:21 +0100

    Declaration des variables

---
+++ script.js
@@ -0,0 +1,26 @@
+/** Declaration des Variables */
+
+const lightbtn = document.getElementById("lightmode");
+const darkbtn = document.getElementById("darkmode");
+
+const srchbar = document.getElementById("default-search");
+const srchbtn = document.getElementById("searchrbt");
+
+const nbrrepo = document.getElementById("reposcnt");
+const nbrflwrs = document.getElementById("flwrscnt");
+const nbrflwng = document.getElementById("flwngcnt");
+
+const imgprfl = document.getElementById("prflimg");
+
+const username = document.getElementById("usergitname");
+const country = document.getElementById("usercntry");
+const addressgit = document.getElementById("useraddrs");
+const gitcpny = document.getElementById("usercpny");
+
+const repovst = document.getElementById("repovst");
+
+const userbio = document.getElementById("userbio");
+
+
+
+
